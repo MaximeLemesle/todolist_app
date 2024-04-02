e1d39ac443226e79c3e12ba097c28cf8c7ab738d
Maxime Lemesle <maxime.lemesle7@gmail.com>
2024-04-02 16:55:53 +0200

    🚧 Save error, try json file | 02/04

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-import { app, BrowserWindow } from "electron";
-// import icon from "./src/images/logo-todolist.png?asset"
+import { app, BrowserWindow, ipcMain } from "electron";
+// import icon from "./src/images/logo-todolist.png?asset";
+import path from "path";
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 600,
+    // width: 600,
+    width: 900,
     height: 800,
     autoHideMenuBar: true,
     // icon: icon,
@@ -13,16 +15,11 @@ const createWindow = () => {
     },
   });
 
-  win.loadFile("./src/index.html");
+  // Affiche les devsTools
   win.webContents.openDevTools();
-};
 
-try {
-  require("electron-reloader")(module, {
-    debug: true,
-    watchRenderer: true,
-  });
-} catch (_) {}
+  win.loadFile("./src/index.html");
+};
 
 app.whenReady().then(() => {
   createWindow();
@@ -30,6 +27,7 @@ app.whenReady().then(() => {
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
+
 });
 
 app.on("window-all-closed", () => {
--- src/js/todo-list.js
+++ src/js/todo-list.js
@@ -1,49 +1,55 @@
-import Store from "electron-store";
-import { generateId } from "./utils";
-
-const store = new Store();
+import { generateId } from "./utils.js";
 
 const input = document.querySelector("#input");
 const button = document.querySelector("#add_task");
 const list = document.querySelector("#list");
 
-let task = "";
+let tasks = [];
 const check = document.querySelectorAll(".check");
 
 // Ajout d'une tache
-function addTask() {
+function addTask(task) {
+  const id = generateId();
+
+  tasks.push({ id, task, checked: false });
+
   if (input.value !== "") {
     task = input.value;
 
     const id = generateId();
 
     list.innerHTML += `
-        <li class="item" id="${id}">
-            <div class="check"></div>
-            <div class="content">
-                <p class="text">${task}</p>
-            </div>
-            <img class="icon" src="assets/poubelle.png" alt="delete task">
-        </li>`;
-
-    store.set(id, {
-      task,
-      checked: false
-    })
+      <li class="item" id="${id}">
+          <div class="check"></div>
+          <div class="content">
+              <p class="text">${task}</p>
+          </div>
+          <img class="icon" src="images/poubelle.png" alt="delete task">
+      </li>`;
 
     input.value = "";
+
+    saveTasksInJson();
   }
 }
 
-button.addEventListener("click", addTask);
+// Ajout d'une tache avec le btn "ajouter"
+button.addEventListener("click", () => {
+  if (input.value.trim() !== "") {
+    addTask(input.value.trim());
+    input.value = "";
+  }
+});
+
+// Ajout d'une tache avec la touche entrer
 input.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-        addTask();
-    }
+  if (event.key === "Enter" && input.value.trim() !== "") {
+    addTask(input.value.trim());
+    input.value = "";
+  }
 });
 
 // Détecter les changements
-
 function callback(list) {
   for (let i = 0; i < list.length; i++) {
     const elements = list[i].addedNodes;
@@ -51,9 +57,9 @@ function callback(list) {
       element.addEventListener("click", () => {
         const checked = element.getAttribute("checked");
 
-        if(checked) {
-          element.removeAttribute("checked")
-          fs.writeFile
+        if (checked) {
+          element.removeAttribute("checked");
+          fs.writeFile;
         } else {
           element.setAttribute("checked", true);
         }
@@ -61,11 +67,26 @@ function callback(list) {
     });
   }
 }
-
 const observer = new MutationObserver(callback);
 observer.observe(list, { attributes: true, childList: true, subtree: true });
 
+// Sauvegarder les taches
+const saveTasksInJson = () => {
+  console.log(tasks);
+  window.Bridge.saveData(tasks);
+};
 
-
-// Problème :
-// electron n'accède pas à node_modules, uniquement src, donc electron-store ne fonctionne pas
+// Afficher les taches sauvegardées
+// window.addEventListener("load", () => {
+//   const savedTasks = window.Bridge.loadData();
+//   if (savedTasks) {
+//     tasks = JSON.parse(savedTasks);
+//     tasks.forEach((task) => {
+//       addTask(task.task);
+//       if (task.checked) {
+//         const listItem = document.getElementById(task.id);
+//         listItem.querySelector(".check").classList.add("checked");
+//       }
+//     });
+//   }
+// });
